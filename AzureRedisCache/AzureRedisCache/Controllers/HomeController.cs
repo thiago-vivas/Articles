1c371b41be826c446766c1b131d9a66b9eed545e
Thiago Araujo <thiago.araujo@outlook.com>
2017-10-11 18:19:08 -0300

    Initial Commit

---
+++ AzureRedisCache/AzureRedisCache/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace AzureRedisCache.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDistributedCache _cache;
+
+        public HomeController(IDistributedCache cache)
+        {
+            this._cache = cache;
+        }
+        public IActionResult Index()
+        {
+           string test = _cache.GetString("Test") ?? "";
+            if (string.IsNullOrEmpty( test))
+            {
+                _cache.SetString("Test", "Tested");
+                test = JsonConvert.DeserializeObject<string>(_cache.GetString("Test") ?? "");
+            }
+            ViewData["Test"] = test;
+            return View();
+        }
+
+    }
+}
