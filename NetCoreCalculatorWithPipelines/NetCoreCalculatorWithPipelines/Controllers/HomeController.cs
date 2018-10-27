69e232d8d464066df3acb43a5ed89eb3fd39eef4
Thiago Araujo <thiago.vivas@outlook.com>
2018-10-27 11:07:07 +0100

    First Commit

---
+++ NetCoreCalculatorWithPipelines/NetCoreCalculatorWithPipelines/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreCalculator.Models;
+
+namespace NetCoreCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index( Operation model )
+        {
+            if ( model.OperationType == OperationType.Addition )
+                model.Result = model.NumberA + model.NumberB;
+            return View( model );
+        }
+    }
+}
