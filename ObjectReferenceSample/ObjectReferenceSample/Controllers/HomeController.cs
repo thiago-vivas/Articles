4b5198ace89f9dfcd2e73b855d15d1af1cd74203
thiago-vivas <thiago.vivas@outlook.com>
2018-11-10 14:33:37 +0000

    Initial Commit

---
+++ ObjectReferenceSample/ObjectReferenceSample/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ObjectReferenceSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+        public IActionResult NewObject()
+        {
+            return View();
+        }
+
+        public IActionResult ConditionStatement()
+        {
+            return View();
+        }
+        public IActionResult ObjectInsideObject()
+        {
+            return View();
+        }
+        public IActionResult BoxedValue()
+        {
+            return View();
+        }
+        public IActionResult AddInNullList()
+        {
+            return View();
+        }
+    }
+}
