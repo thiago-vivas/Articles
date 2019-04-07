7242c63bd17d32853297fbf57c57b8b8cefebef8
Thiago Araujo <thiago.vivas@outlook.com>
2019-04-07 21:54:30 +0100

    Initial Commit

---
+++ WebApiWithSwagger/WebApiWithSwagger/Models/ValueSamples.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiWithSwagger.Models
+{
+    public static class ValueSamples
+    {
+        public static Dictionary<int, string> MyValue;
+
+        public static void Initialize()
+        {
+            MyValue = new Dictionary<int, string>();
+            MyValue.Add( 0, "Value 0" );
+            MyValue.Add( 1, "Value 1" );
+            MyValue.Add( 2, "Value 2" );
+        }
+    }
+}
