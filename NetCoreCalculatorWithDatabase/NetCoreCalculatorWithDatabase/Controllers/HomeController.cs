63adb5ff3ef27b29bf2113498f7019beca0f375a
Thiago Vivas de Araujo <thiago-v-araujo@telecom.pt>
2019-07-09 20:24:21 +0100

    Initial Commit

---
+++ NetCoreCalculatorWithDatabase/NetCoreCalculatorWithDatabase/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreCalculator.Business;
+using NetCoreCalculator.Models;
+
+namespace NetCoreCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index( Operation model )
+        {
+            if ( model.OperationType == OperationType.Addition )
+                model.Result = model.NumberA + model.NumberB;
+            return View( model );
+        }
+
+        [HttpPost]
+        public IActionResult CompoundInterest( CompoundInterestModel model )
+        {
+            model.Result = Calculation.CalculateCompoundInterest( model );
+            return View( model );
+        }
+
+        [HttpGet]
+        public IActionResult CompoundInterest()
+        {
+            return View();
+        }
+    }
+}
---
+++ NetCoreCalculatorWithDatabase/NetCoreCalculatorWithDatabase/Models/OperationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreCalculator.Models
+{
+    public enum OperationType
+    {
+        Addition,
+        Multiplication,
+        Division,
+        Subtraction
+    }
+}
