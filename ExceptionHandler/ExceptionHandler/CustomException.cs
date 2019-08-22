8054a0efcbd1179a7638801b897271626e750f69
Thiago Vivas de Araujo <thiago.vivas@outlook.com>
2019-08-22 21:16:32 +0100

    Initial commit

---
+++ ExceptionHandler/ExceptionHandler/CustomException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandler
+{
+    public class CustomException : Exception
+    {
+        public CustomException(): base("This is a custom exception")
+        {
+
+        }
+    }
+}
---
+++ ExceptionHandler/ExceptionHandler/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Runtime.CompilerServices;
+
+namespace ExceptionHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static void CustomException()
+        {
+            try
+            {
+                throw new CustomException();
+            }
+            catch (CustomException ex) //must comes first
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void ArithmeticException()
+        {
+            int division = 0;
+
+            try
+            {
+                var throwException = 15 / division;
+            }
+            catch (ArithmeticException ex) //must comes first
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static void NullReferenceException()
+        {
+            List<int> nullIntegerList = null;
+            try
+            {
+                var throwException = nullIntegerList.Count;
+            }
+            catch (NullReferenceException ex) //must comes first
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        public static void FileNotFoundException()
+        {
+            try
+            {
+                var throwException = File.Open(@"C:\notExistentFile.jpg", FileMode.Open);
+            }
+            catch (FileNotFoundException ex) //must comes first
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        public static void IndexOutOfRangeException()
+        {
+            int[] sampleArray = new int[3] { 1, 2, 3 };
+            try
+            {
+                var throwException = sampleArray[3];
+            }
+            catch (IndexOutOfRangeException ex) //must comes first
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
