384596d2db78845527af04c14d0294504654418a
Thiago Vivas de Araujo <thiago.vivas@outlook.com>
2019-10-13 15:59:03 +0100

    Sample Command added

--- SampleVisualStudioExtension/SampleVisualStudioExtensionPackage.cs
+++ SampleVisualStudioExtension/SampleVisualStudioExtensionPackage.cs
@@ -25,6 +25,7 @@ namespace SampleVisualStudioExtension
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(SampleVisualStudioExtensionPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class SampleVisualStudioExtensionPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace SampleVisualStudioExtension
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await SampleCommand.InitializeAsync(this);
         }
 
         #endregion
