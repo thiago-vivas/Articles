8c639daa657ceb32e045f4c8fc25dd712f637b35
Thiago Vivas de Araujo <thiago-v-araujo@telecom.pt>
2020-02-02 15:53:22 +0000

    Final commit working

--- AzureVirtualMachine/Program.cs
+++ AzureVirtualMachine/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Azure.Management.Compute.Fluent.Models;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +14,65 @@ namespace AzureVirtualMachine
     {
         static void Main(string[] args)
         {
+            var credentials = SdkContext.AzureCredentialsFactory
+                .FromServicePrincipal("clientId", "clientSecret", "tenantId", AzureEnvironment.AzureGlobalCloud);
+
+
+            var azure = Azure
+                .Configure()
+                .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+                .Authenticate(credentials)
+                .WithDefaultSubscription();
+
+
+            var groupName = "sampleResourceGroup";
+            var vmName = "VMWithCSharp";
+            var location = Region.EuropeWest;
+
+
+            var resourceGroup = azure.ResourceGroups.Define(groupName)
+                .WithRegion(location)
+                .Create();
+
+            var network = azure.Networks.Define("sampleVirtualNetwork")
+              .WithRegion(location)
+              .WithExistingResourceGroup(groupName)
+              .WithAddressSpace("10.0.0.0/16")
+              .WithSubnet("sampleSubNet", "10.0.0.0/24")
+              .Create();
+
+            var publicIPAddress = azure.PublicIPAddresses.Define("samplePublicIP")
+                .WithRegion(location)
+                .WithExistingResourceGroup(groupName)
+                .WithDynamicIP()
+                .Create();
+
+            var networkInterface = azure.NetworkInterfaces.Define("sampleNetWorkInterface")
+                .WithRegion(location)
+                .WithExistingResourceGroup(groupName)
+                .WithExistingPrimaryNetwork(network)
+                .WithSubnet("sampleSubNet")
+                .WithPrimaryPrivateIPAddressDynamic()
+                .WithExistingPrimaryPublicIPAddress(publicIPAddress)
+                .Create();
+
+            var availabilitySet = azure.AvailabilitySets.Define("sampleAvailabilitySet")
+                .WithRegion(location)
+                .WithExistingResourceGroup(groupName)
+                .WithSku(AvailabilitySetSkuTypes.Aligned) 
+                .Create();          
+
+            azure.VirtualMachines.Define(vmName)
+                .WithRegion(location)
+                .WithExistingResourceGroup(groupName)
+                .WithExistingPrimaryNetworkInterface(networkInterface)                
+                .WithLatestWindowsImage("MicrosoftWindowsServer", "WindowsServer", "2012-R2-Datacenter")
+                .WithAdminUsername("sampleUser")
+                .WithAdminPassword("Sample123467")
+                .WithComputerName(vmName)
+                .WithExistingAvailabilitySet(availabilitySet)
+                .WithSize(VirtualMachineSizeTypes.StandardB1s)
+                .Create();
         }
     }
 }
