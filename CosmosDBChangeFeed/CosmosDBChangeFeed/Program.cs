eaf5a4072fe072cc3f36769b5a8a83e57b068b0f
Thiago Araujo <thiagoaraujo@boldint.com>
2020-11-04 21:19:46 +0000

    Initial Commit

---
+++ CosmosDBChangeFeed/CosmosDBChangeFeed/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosmosDBChangeFeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
