a804657fe83d91d9d13c8396ccdd68e1ede724d8
Thiago Araujo <thiagoaraujo@boldint.com>
2020-11-15 16:30:24 +0000

    Wip

---
+++ NetCoreCalculatorWithKeyVault/NetCoreCalculator/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using NetCoreCalculator.Business;
+using NetCoreCalculator.Models;
+
+namespace NetCoreCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string keyVaultURI = "https://azurekeyvaultsampletwo.vault.azure.net/";
+
+        private readonly string clientAppID = "";
+
+        private readonly string clientAppSecret = "";
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            ClientCredential credential = new ClientCredential(clientAppID, clientAppSecret);
+            KeyVaultClient.AuthenticationCallback authenticationCallback = new
+                KeyVaultClient.AuthenticationCallback(async
+                (string authenticationAuthority, string resource, string scope) =>
+            {
+                AuthenticationContext authenticationContext = new AuthenticationContext(authenticationAuthority);
+                AuthenticationResult result = await authenticationContext.AcquireTokenAsync(resource, credential);
+                return result.AccessToken;
+            });
+            KeyVaultClient client = new KeyVaultClient(authenticationCallback);
+            var secret = await client.GetSecretAsync(keyVaultURI, "sampleSecret");
+
+            ViewBag.Secret = secret.Value;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Operation model)
+        {
+            if (model.OperationType == OperationType.Addition)
+                model.Result = model.NumberA + model.NumberB;
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult CompoundInterest(CompoundInterestModel model)
+        {
+            model.Result = Calculation.CalculateCompoundInterest(model);
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult CompoundInterest()
+        {
+            return View();
+        }
+    }
+}
---
+++ NetCoreCalculatorWithKeyVault/NetCoreCalculator/Models/CompoundInterestModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreCalculator.Models
+{
+    public class CompoundInterestModel
+    {
+        [Display( Name = "Result - A" )]
+        public double Result { get; set; }
+
+        [Display( Name = "Initial Investment - P" )]
+        public double Investment { get; set; }
+
+        [Display( Name = "Interest Rate - r" )]
+        public double InterestRate { get; set; }
+
+        [Display( Name = "Period of Time - n" )]
+        public int TimePeriod { get; set; }
+    }
+}
