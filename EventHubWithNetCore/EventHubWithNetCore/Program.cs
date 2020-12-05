d4344801eedd9db903c488eb4f14e026017845c3
Thiago Araujo <thiagoaraujo@boldint.com>
2020-12-05 19:09:25 +0000

    Initial Commit

---
+++ EventHubWithNetCore/EventHubWithNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventHubWithNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
