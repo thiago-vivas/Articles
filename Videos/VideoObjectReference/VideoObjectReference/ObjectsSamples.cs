b9d8762e3addffd75b8d1bb2f59b753d12580740
Thiago Araujo <thiago.vivas@outlook.com>
2021-02-10 10:36:47 +0000

    Initial Commit

---
+++ Videos/VideoObjectReference/VideoObjectReference/ObjectsSamples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoObjectReference
+{
+    public class SampleObj
+    {
+        public string Item1 { get; set; }
+        public SampleChildObj ChildObj { get; set; }
+    }
+    public class SampleChildObj
+    {
+        public string Item2 { get; set; }
+    }
+}
---
+++ Videos/VideoObjectReference/VideoObjectReference/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VideoObjectReference
+{
+    class Program
+    {
+        static void  Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ErrorSamples samples = new ErrorSamples(null);
+            samples.Run();
+        }
+    }
+}
