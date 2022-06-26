0a884c6321ce38dab0692e3331d810d291893a0f
Thiago Vivas de Araujo <thiago-v-araujo@telecom.pt>
2022-06-26 13:22:17 +0100

    Final commit

---
+++ NCacheSignalR/NCacheHelper/SampleHub.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace NCacheSignalR.NCacheHelper
+{
+    public class SampleHub : Hub
+    {
+        public Task SendMessageToAll(string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", message);
+        }
+
+        public Task SendMessageToCaller(string message)
+        {
+            return Clients.Caller.SendAsync("ReceiveMessage", message);
+        }
+
+        public Task SendMessageToUser(string connectionId, string message)
+        {
+            return Clients.Client(connectionId).SendAsync("ReceiveMessage", message);
+        }
+
+        public Task JoinGroup(string group)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public Task SendMessageToGroup(string group, string message)
+        {
+            return Clients.Group(group).SendAsync("ReceiveMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+            await base.OnDisconnectedAsync(ex);
+        }
+    }
+}
---
+++ NCacheSignalR/wwwroot/js/chat.js
@@ -0,0 +1,63 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/messages").build();
+
+connection.on("ReceiveMessage", function (message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var div = document.createElement("div");
+    div.innerHTML = msg + "<hr/>";
+    document.getElementById("messages").appendChild(div);
+});
+
+connection.on("UserConnected", function (connectionId) {
+    var groupElement = document.getElementById("group");
+    var option = document.createElement("option");
+    option.text = connectionId;
+    option.value = connectionId;
+    groupElement.add(option);
+});
+
+connection.on("UserDisconnected", function (connectionId) {
+    var groupElement = document.getElementById("group");
+    for (var i = 0; i < groupElement.length; i++) {
+        if (groupElement.options[i].value == connectionId) {
+            groupElement.remove(i);
+        }
+    }
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var message = document.getElementById("message").value;
+    var user = document.getElementById("userInput").value;
+    var groupElement = document.getElementById("group");
+    var groupValue = groupElement.options[groupElement.selectedIndex].value;
+    var finalMessage = `${user} says ${message}`;
+
+    if (groupValue === "All" || groupValue === "Myself") {
+        var method = groupValue === "All" ? "SendMessageToAll" : "SendMessageToCaller";
+        connection.invoke(method, finalMessage).catch(function (err) {
+            return console.error(err.toString());
+        });
+    } else if (groupValue === "PrivateGroup") {
+        connection.invoke("SendMessageToGroup", "PrivateGroup", finalMessage).catch(function (err) {
+            return console.error(err.toString());
+        });
+    } else {
+        connection.invoke("SendMessageToUser", groupValue, finalMessage).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+
+    event.preventDefault();
+});
+
+document.getElementById("joinGroup").addEventListener("click", function (event) {
+    connection.invoke("JoinGroup", "PrivateGroup").catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
