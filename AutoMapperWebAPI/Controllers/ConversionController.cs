f58cd3b566c5f6e0d7a3db8ea3f188a7857d3258
Thiago <thiago-v-araujo@telecom.pt>
2022-10-16 14:59:58 +0100

    initial commit

---
+++ AutoMapperWebAPI/Controllers/ConversionController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapperWebAPI.DTOs;
+using AutoMapperWebAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoMapperWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversionController : ControllerBase
+    {
+        [HttpPost(Name = "FromCategoryDtoToCategoryEntity")]
+        public Category PostFromCategoryDtoToCategoryEntity([FromBody] CategoryDto category)
+        {
+            return new Category();
+        }
+        [HttpPost(Name = "FromCategoryEntityToCategoryDto")]
+        public CategoryDto PostFromCategoryEntityToCategoryDto([FromBody] Category category)
+        {
+            return new CategoryDto();
+        }
+        [HttpPost(Name = "FromProductDtoToProductEntity")]
+        public Product PostFromProductDtoToProductEntity([FromBody] ProductDto category)
+        {
+            return new Product();
+        }
+        [HttpPost(Name = "FromProductEntityToProductDto")]
+        public ProductDto PostFromProductEntityToProductDto([FromBody] Product category)
+        {
+            return new ProductDto();
+        }
+    }
+}
---
+++ AutoMapperWebAPI/DTOs/CategoryDto.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapperWebAPI.Entities;
+
+namespace AutoMapperWebAPI.DTOs
+{
+    public class CategoryDto
+    {
+        public CategoryDto()
+        {
+            Products = new HashSet<ProductDto>();
+        }
+
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public byte[]? Picture { get; set; }
+
+        public virtual ICollection<ProductDto> Products { get; set; }
+    }
+}
