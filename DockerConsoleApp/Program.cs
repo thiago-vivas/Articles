b7a79871ded3628aa2f59e847750e320c3eb08b3
Thiago Vivas de Araujo <thiago.vivas@outlook.com>
2022-12-26 22:34:56 +0000

    Add files via upload

--- DockerConsoleApp/Program.cs
+++ DockerConsoleApp/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine(DateTime.Now.ToLongDateString());
+
+Console.WriteLine("App closing in 10s");
+Console.WriteLine("10");
+await Task.Delay(1000);
+
+Console.WriteLine("9");
+await Task.Delay(1000);
+
+Console.WriteLine("8");
+await Task.Delay(1000);
+
+Console.WriteLine("7");
+await Task.Delay(1000);
+
+Console.WriteLine("6");
+await Task.Delay(1000);
+
+Console.WriteLine("5");
+await Task.Delay(1000);
+
+Console.WriteLine("4");
+await Task.Delay(1000);
+
+Console.WriteLine("3");
+await Task.Delay(1000);
+
+Console.WriteLine("2");
+await Task.Delay(1000);
+
+Console.WriteLine("1");
+await Task.Delay(1000);
